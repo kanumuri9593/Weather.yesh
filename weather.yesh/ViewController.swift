3d051ade9310e0a5b429633b5484c90dc0062487
Yeswanth varma Kanumuri <yeshu@Yeswanths-MBP.fios-router.home>
2016-01-11 19:44:03 -0500

    alomifire installed

--- weather.yesh/ViewController.swift
+++ weather.yesh/ViewController.swift
@@ -8,7 +8,37 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var CityLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    
+    @IBOutlet weak var HighTempLbl: UILabel!
+    
+    @IBOutlet weak var LowTempLbl: UILabel!
+    
+    @IBOutlet weak var climateConditionLbl: UILabel!
+    
+    @IBOutlet weak var pressureLbl: UILabel!
+    
+    @IBOutlet weak var HumidityLbl: UILabel!
+    
+    @IBOutlet weak var WindSpeedLbl: UILabel!
+    
+    @IBOutlet weak var WindDegLbl: UILabel!
+    
+    @IBOutlet weak var cloudLbl: UILabel!
+    
+    @IBOutlet weak var sunriseLbl: UILabel!
+    
+    @IBOutlet weak var sunsetLbl: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +50,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBOutlet weak var searchButton: UIStackView!
+    
+    @IBAction func currentLocationButn(sender: AnyObject) {
+    }
+    
+    @IBAction func searchButn(sender: AnyObject) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
---
+++ weather.yesh/character.swift
@@ -0,0 +1,17 @@
+//
+//  character.swift
+//  weather.yesh
+//
+//  Created by Yeswanth varma Kanumuri on 1/10/16.
+//  Copyright © 2016 Yeswanth varma Kanumuri. All rights reserved.
+//
+
+import Foundation
+
+
+class  Character {
+    // http://api.openweathermap.org/data/2.5/weather?q=Rajahmundry&APPID=cfce87ab2754c8f6b9545c8b95910423
+    
+    
+    
+}
